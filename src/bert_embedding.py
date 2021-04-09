55971bfedff081bc18e81b04fa9346f025fe56dd
zycalice <alicezhang727@gmail.com>
2021-04-09 09:25:30 -0400

    ove bert embedding functions to bert embedding.py

--- src/bert_embedding.py
+++ src/bert_embedding.py
@@ -11,6 +11,20 @@ def output_bert_embeddings(domain_types, data_size):
                                            data_path + "all_bert/" + "encoded_" + domain + "_dev_" + str(data_size))
 
 
+def tokenize_encode_bert_sentences(tokenizer, model, input_sentences, output_path):
+    output = np.zeros([len(input_sentences), 768])
+    for i, x in enumerate(input_sentences):
+        output[i] = tokenize_encode_bert_sentences_sample(tokenizer, model, [x])
+    np.save(output_path, output)
+    return output
+
+
+def tokenize_encode_bert_sentences_sample(tokenizer, model, input_sentences):
+    encoded_input = tokenizer(input_sentences, return_tensors='pt', truncation=True, padding=True)
+    output = model(**encoded_input)[0][:, 0, :].detach().numpy()
+    return output
+
+
 if __name__ == '__main__':
     data_path = "../data/"
     tokenizer_d = DistilBertTokenizer.from_pretrained('distilbert-base-uncased')
--- src/utils.py
+++ src/utils.py
@@ -31,22 +31,6 @@ def load_bert(data_path, domains, data_size):
     return bert_dataset
 
 
-# BERT embedding functions.
-
-def tokenize_encode_bert_sentences(tokenizer, model, input_sentences, output_path):
-    output = np.zeros([len(input_sentences), 768])
-    for i, x in enumerate(input_sentences):
-        output[i] = tokenize_encode_bert_sentences_sample(tokenizer, model, [x])
-    np.save(output_path, output)
-    return output
-
-
-def tokenize_encode_bert_sentences_sample(tokenizer, model, input_sentences):
-    encoded_input = tokenizer(input_sentences, return_tensors='pt', truncation=True, padding=True)
-    output = model(**encoded_input)[0][:, 0, :].detach().numpy()
-    return output
-
-
 # Pseudo labeling (self train) and gradual train.
 
 def psuedo_labeling(X_source, y_source, X_ti, y_ti, model, conf=0):
