d8e1cf14d3a9d0e4704d6a7f5e671048010ba09c
zycalice <alicezhang727@gmail.com>
2021-05-28 11:09:21 -0400

    edit literatures

--- literatures.md
+++ literatures.md
@@ -3,3 +3,6 @@
 * 
 ## Domain alignment
 * 
+
+# Tutorials
+* [NER with BERT](https://medium.com/@yingbiao/ner-with-bert-in-action-936ff275bc73#:~:text=NER%20is%20a%20task%20in,model%20for%20NER%20downstream%20task.)
--- src/ner_mc.py
+++ src/ner_mc.py
@@ -8,3 +8,4 @@ from sklearn.model_selection import train_test_split
 from src.domain_space_alignment import ht_lr
 import sys
 
+# https://medium.com/@yingbiao/ner-with-bert-in-action-936ff275bc73#:~:text=NER%20is%20a%20task%20in,model%20for%20NER%20downstream%20task.
